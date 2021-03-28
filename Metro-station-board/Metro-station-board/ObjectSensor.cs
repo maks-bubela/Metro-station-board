75b3a375796bfb75db6fece0dd6d9a44a78312c7
Maks Bubela <ya.bubela@gmail.com>
2021-03-28 22:31:42 +0300

    Add 2 new methods startRide and finisRide and replace class to interface

--- Metro-station-board/Metro-station-board/ObjectSensor.cs
+++ Metro-station-board/Metro-station-board/ObjectSensor.cs
@@ -8,16 +8,29 @@ namespace Metro_station_board
 {
     class ObjectSensor
     {
-        private ObjectTrain train;
+
+        private IObjectTransport transport;
         private long timer;
 
         // The time of needed arrival
         private int arrive_time;
 
-        public ObjectSensor(int endHours,int endMin,int endSec, ObjectTrain train)
+        public IObjectTransport GetTransport() { return transport; }
+        public int GetArriveTime() { return arrive_time; }
+
+        public ObjectSensor(int endHours,int endMin,int endSec, IObjectTransport transport)
         {
-            this.train = train;
+            this.transport = transport;
             this.arrive_time = (60 * 60 * endHours) + (60 * endMin) + endSec;
+        }
+
+        public void startRide()
+        {
+            this.timer = DateTime.Now.Ticks;
+        }
+        public long finishRide()
+        {
+            return (DateTime.Now.Ticks - timer);
         }
     }
 }
