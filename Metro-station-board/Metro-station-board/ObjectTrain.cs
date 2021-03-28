f6b46492c44a76dfa103b2da827f2f66ba070f1f
Maks Bubela <ya.bubela@gmail.com>
2021-03-28 17:01:41 +0300

    Added needed fields in ObjectTrain and added conctructor

--- Metro-station-board/Metro-station-board/ObjectTrain.cs
+++ Metro-station-board/Metro-station-board/ObjectTrain.cs
@@ -6,5 +6,15 @@ namespace Metro_station_board
 {
     class ObjectTrain
     {
+        private string dispatchPoint;
+        private string endPoint;
+        private bool trainInTheWay;
+
+        public ObjectTrain(string dispatchPoint, string endPoint, bool trainInTheWay)
+        {
+            this.dispatchPoint = dispatchPoint;
+            this.endPoint = endPoint;
+            this.trainInTheWay = trainInTheWay;
+        }
     }
 }
