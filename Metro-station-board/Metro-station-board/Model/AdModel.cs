931d0d4c01b075a1169a1a271a6f283151f91c49
Maks Bubela <ya.bubela@gmail.com>
2021-03-29 13:52:13 +0300

    Added all tables database models

---
+++ Metro-station-board/Metro-station-board/Model/AdModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metro_station_board.Model
+{
+    class AdModel
+    {
+        public long Id { get; set; }
+        public string ad { get; set; }
+    }
+}
---
+++ Metro-station-board/Metro-station-board/Model/ScheduleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metro_station_board.Model
+{
+    class ScheduleModel
+    {
+        public long Id { get; set; }
+        public TimeSpan arriveTime { get; set; }
+    }
+}
