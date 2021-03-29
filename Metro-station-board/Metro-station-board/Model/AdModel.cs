a81e61f9ae2833ed4951a97a73b7e5e0ad3710a0
Maks Bubela <ya.bubela@gmail.com>
2021-03-29 14:50:40 +0300

    Inherited from IModel interface

--- Metro-station-board/Metro-station-board/Model/AdModel.cs
+++ Metro-station-board/Metro-station-board/Model/AdModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Metro_station_board.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Metro_station_board.Model
 {
-    public class AdModel
+    public class AdModel : IModel
     {
         public long Id { get; set; }
         public string ad { get; set; }
--- Metro-station-board/Metro-station-board/Model/ViolationModel.cs
+++ Metro-station-board/Metro-station-board/Model/ViolationModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Metro_station_board.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Metro_station_board.Model
 {
-    public class ViolationModel
+    public class ViolationModel : IModel
     {
         public long Id { get; set; }
         public TimeSpan violationTime { get; set; }
