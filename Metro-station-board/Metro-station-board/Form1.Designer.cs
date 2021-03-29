fe953c4db61a117f05cd4fe6fe08f0594a99f8e1
Maks Bubela <ya.bubela@gmail.com>
2021-03-29 14:19:29 +0300

    Replace project to .Net FrameWork

--- Metro-station-board/Metro-station-board/Form1.Designer.cs
+++ Metro-station-board/Metro-station-board/Form1.Designer.cs
@@ -4,14 +4,14 @@ namespace Metro_station_board
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,115 +21,118 @@ namespace Metro_station_board
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
             this.contolPanelLabel = new System.Windows.Forms.Label();
+            this.dataOfScheduleLabel = new System.Windows.Forms.Label();
+            this.tableLabel = new System.Windows.Forms.Label();
             this.adSourseLabel = new System.Windows.Forms.Label();
             this.adButton = new System.Windows.Forms.Button();
-            this.dataOfScheduleLabel = new System.Windows.Forms.Label();
-            this.getDataButton = new System.Windows.Forms.Button();
-            this.violationsBox = new System.Windows.Forms.TextBox();
+            this.dataButton = new System.Windows.Forms.Button();
             this.tableButton = new System.Windows.Forms.Button();
-            this.tableLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // contolPanelLabel
             // 
             this.contolPanelLabel.AutoSize = true;
-            this.contolPanelLabel.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.contolPanelLabel.Location = new System.Drawing.Point(268, 18);
+            this.contolPanelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.contolPanelLabel.Location = new System.Drawing.Point(258, 19);
             this.contolPanelLabel.Name = "contolPanelLabel";
-            this.contolPanelLabel.Size = new System.Drawing.Size(243, 50);
+            this.contolPanelLabel.Size = new System.Drawing.Size(250, 44);
             this.contolPanelLabel.TabIndex = 0;
             this.contolPanelLabel.Text = "Control Panel";
-            // 
-            // adSourseLabel
-            // 
-            this.adSourseLabel.AutoSize = true;
-            this.adSourseLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.adSourseLabel.Location = new System.Drawing.Point(49, 103);
-            this.adSourseLabel.Name = "adSourseLabel";
-            this.adSourseLabel.Size = new System.Drawing.Size(253, 38);
-            this.adSourseLabel.TabIndex = 1;
-            this.adSourseLabel.Text = "Set up ad sourse  : ";
-            // 
-            // adButton
-            // 
-            this.adButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.adButton.Location = new System.Drawing.Point(81, 144);
-            this.adButton.Name = "adButton";
-            this.adButton.Size = new System.Drawing.Size(170, 63);
-            this.adButton.TabIndex = 2;
-            this.adButton.Text = "Set up sourse";
-            this.adButton.UseVisualStyleBackColor = true;
-            this.adButton.Click += new System.EventHandler(this.adButton_Click);
+            this.contolPanelLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // dataOfScheduleLabel
             // 
             this.dataOfScheduleLabel.AutoSize = true;
-            this.dataOfScheduleLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.dataOfScheduleLabel.Location = new System.Drawing.Point(431, 103);
+            this.dataOfScheduleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataOfScheduleLabel.Location = new System.Drawing.Point(411, 97);
             this.dataOfScheduleLabel.Name = "dataOfScheduleLabel";
-            this.dataOfScheduleLabel.Size = new System.Drawing.Size(379, 38);
-            this.dataOfScheduleLabel.TabIndex = 3;
+            this.dataOfScheduleLabel.Size = new System.Drawing.Size(386, 32);
+            this.dataOfScheduleLabel.TabIndex = 1;
             this.dataOfScheduleLabel.Text = "Data on schedule violations : ";
-            // 
-            // getDataButton
-            // 
-            this.getDataButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.getDataButton.Location = new System.Drawing.Point(538, 144);
-            this.getDataButton.Name = "getDataButton";
-            this.getDataButton.Size = new System.Drawing.Size(170, 63);
-            this.getDataButton.TabIndex = 4;
-            this.getDataButton.Text = "Get data";
-            this.getDataButton.UseVisualStyleBackColor = true;
-            // 
-            // violationsBox
-            // 
-            this.violationsBox.Location = new System.Drawing.Point(272, 144);
-            this.violationsBox.Multiline = true;
-            this.violationsBox.Name = "violationsBox";
-            this.violationsBox.Size = new System.Drawing.Size(243, 138);
-            this.violationsBox.TabIndex = 5;
-            // 
-            // tableButton
-            // 
-            this.tableButton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tableButton.Location = new System.Drawing.Point(300, 353);
-            this.tableButton.Name = "tableButton";
-            this.tableButton.Size = new System.Drawing.Size(170, 63);
-            this.tableButton.TabIndex = 7;
-            this.tableButton.Text = "Table Forms";
-            this.tableButton.UseVisualStyleBackColor = true;
+            this.dataOfScheduleLabel.Click += new System.EventHandler(this.label2_Click);
             // 
             // tableLabel
             // 
             this.tableLabel.AutoSize = true;
-            this.tableLabel.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tableLabel.Location = new System.Drawing.Point(268, 312);
+            this.tableLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tableLabel.Location = new System.Drawing.Point(260, 297);
             this.tableLabel.Name = "tableLabel";
-            this.tableLabel.Size = new System.Drawing.Size(247, 38);
-            this.tableLabel.TabIndex = 6;
+            this.tableLabel.Size = new System.Drawing.Size(246, 32);
+            this.tableLabel.TabIndex = 4;
             this.tableLabel.Text = "Go to table forms :";
+            // 
+            // adSourseLabel
+            // 
+            this.adSourseLabel.AutoSize = true;
+            this.adSourseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.adSourseLabel.Location = new System.Drawing.Point(24, 97);
+            this.adSourseLabel.Name = "adSourseLabel";
+            this.adSourseLabel.Size = new System.Drawing.Size(257, 32);
+            this.adSourseLabel.TabIndex = 5;
+            this.adSourseLabel.Text = "Set up ad sourse  : ";
+            this.adSourseLabel.Click += new System.EventHandler(this.adSourseLabel_Click);
+            // 
+            // adButton
+            // 
+            this.adButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.adButton.Location = new System.Drawing.Point(67, 132);
+            this.adButton.Name = "adButton";
+            this.adButton.Size = new System.Drawing.Size(159, 71);
+            this.adButton.TabIndex = 6;
+            this.adButton.Text = "Set up sourse";
+            this.adButton.UseVisualStyleBackColor = true;
+            // 
+            // dataButton
+            // 
+            this.dataButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataButton.Location = new System.Drawing.Point(527, 132);
+            this.dataButton.Name = "dataButton";
+            this.dataButton.Size = new System.Drawing.Size(159, 71);
+            this.dataButton.TabIndex = 7;
+            this.dataButton.Text = "Get Data";
+            this.dataButton.UseVisualStyleBackColor = true;
+            this.dataButton.Click += new System.EventHandler(this.dataButton_Click);
+            // 
+            // tableButton
+            // 
+            this.tableButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tableButton.Location = new System.Drawing.Point(297, 332);
+            this.tableButton.Name = "tableButton";
+            this.tableButton.Size = new System.Drawing.Size(159, 71);
+            this.tableButton.TabIndex = 8;
+            this.tableButton.Text = "Get Table";
+            this.tableButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 132);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 150);
+            this.textBox1.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.tableButton);
-            this.Controls.Add(this.tableLabel);
-            this.Controls.Add(this.violationsBox);
-            this.Controls.Add(this.getDataButton);
-            this.Controls.Add(this.dataOfScheduleLabel);
+            this.Controls.Add(this.dataButton);
             this.Controls.Add(this.adButton);
             this.Controls.Add(this.adSourseLabel);
+            this.Controls.Add(this.tableLabel);
+            this.Controls.Add(this.dataOfScheduleLabel);
             this.Controls.Add(this.contolPanelLabel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -141,13 +144,13 @@ namespace Metro_station_board
         #endregion
 
         private System.Windows.Forms.Label contolPanelLabel;
+        private System.Windows.Forms.Label dataOfScheduleLabel;
+        private System.Windows.Forms.Label tableLabel;
         private System.Windows.Forms.Label adSourseLabel;
         private System.Windows.Forms.Button adButton;
-        private System.Windows.Forms.Label dataOfScheduleLabel;
-        private System.Windows.Forms.Button getDataButton;
-        private System.Windows.Forms.TextBox violationsBox;
+        private System.Windows.Forms.Button dataButton;
         private System.Windows.Forms.Button tableButton;
-        private System.Windows.Forms.Label tableLabel;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
