234d68595a708fa5caf1a9b5ae954641323bc50e
Maks Bubela <ya.bubela@gmail.com>
2021-03-29 14:35:57 +0300

    Added MetroStationBoard context

---
+++ Metro-station-board/Metro-station-board/Context/MetroStationBoardContext.cs
@@ -0,0 +1,17 @@
+﻿using Metro_station_board.Model;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Metro_station_board.Context
+{
+    public class MetroStationBoardContext : DbContext
+    {
+        public MetroStationBoardContext(string connectionString) : base(connectionString)
+        {
+        }
+        public DbSet<AdModel> adModels { get; set; }
+        public DbSet<ScheduleModel> scheduleModels { get; set; }
+        public DbSet<ViolationModel> violationModels { get; set; }
+    }
+}
