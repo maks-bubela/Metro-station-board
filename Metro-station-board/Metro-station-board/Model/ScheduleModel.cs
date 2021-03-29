955968e25abb5b67c136c62681e5de670a61698b
Maks Bubela <ya.bubela@gmail.com>
2021-03-29 15:55:05 +0300

    Added missed fields in ScheduleModel and ViolationModel

--- Metro-station-board/Metro-station-board/Model/ScheduleModel.cs
+++ Metro-station-board/Metro-station-board/Model/ScheduleModel.cs
@@ -7,6 +7,9 @@ namespace Metro_station_board.Model
     public class ScheduleModel
     {
         public long Id { get; set; }
+        public string dispatchPoint;
+        public string endPoint;
+        public TimeSpan dispatchTime { get; set; }
         public TimeSpan arriveTime { get; set; }
     }
 }
--- Metro-station-board/Metro-station-board/Model/ViolationModel.cs
+++ Metro-station-board/Metro-station-board/Model/ViolationModel.cs
@@ -7,6 +7,9 @@ namespace Metro_station_board.Model
     public class ViolationModel
     {
         public long Id { get; set; }
-        public TimeSpan violationTime { get; set; }
+        public string dispatchPoint;
+        public string endPoint;
+        public TimeSpan dispatchTime { get; set; }
+        public TimeSpan arrivedTime { get; set; }
     }
 }
