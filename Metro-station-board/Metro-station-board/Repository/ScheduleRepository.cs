c2d4518c970b8541303d8c1d07908864eaf6d543
Maks Bubela <ya.bubela@gmail.com>
2021-03-30 14:44:22 +0300

    Add implementation to ScheduleRepository

--- Metro-station-board/Metro-station-board/Repository/ScheduleRepository.cs
+++ Metro-station-board/Metro-station-board/Repository/ScheduleRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Metro_station_board.Context;
+using Metro_station_board.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,37 @@ namespace Metro_station_board.Repository
 {
     class ScheduleRepository
     {
+        private MetroStationBoardContext scheduleContext;
+        public ScheduleRepository(MetroStationBoardContext scheduleContext)
+        {
+            if (scheduleContext != null)
+                this.scheduleContext = scheduleContext;
+        }
+        public void addToDB(TimeSpan arriveTime, TimeSpan dispatchTime,string dispatchPoint, string endPoint)
+        {
+            ScheduleModel scheduleModel = new ScheduleModel 
+            {
+                arriveTime = arriveTime,
+                dispatchTime = dispatchTime,
+                dispatchPoint = dispatchPoint,
+                endPoint = endPoint
+            };
+            scheduleContext.scheduleModels.Add(scheduleModel);
+            scheduleContext.SaveChanges();
+        }
+        public void deleteFromDB(TimeSpan arriveTime, string endPoint)
+        {
+            if (!this.isEmpty())
+            {
+                scheduleContext.scheduleModels.Remove(scheduleContext.scheduleModels
+                    .Where(o => o.arriveTime == arriveTime)
+                    .Where(o => o.endPoint == endPoint).FirstOrDefault());
+                scheduleContext.SaveChanges();
+            }
+        }
+        public bool isEmpty()
+        {
+            return scheduleContext.scheduleModels.Count() == 0;
+        }
     }
 }
