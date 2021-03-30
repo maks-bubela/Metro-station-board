02e08fd5ad4051e323f255cc3971fd7204ed96c5
Maks Bubela <ya.bubela@gmail.com>
2021-03-30 14:32:20 +0300

    Added implementation for AdRepository

--- Metro-station-board/Metro-station-board/Repository/AdRepository.cs
+++ Metro-station-board/Metro-station-board/Repository/AdRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Metro_station_board.Context;
+using Metro_station_board.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,33 @@ namespace Metro_station_board.Repository
 {
     class AdRepository
     {
+        private MetroStationBoardContext adContext;
+        public AdRepository(MetroStationBoardContext adContext)
+        {
+            if (adContext != null)
+                this.adContext = adContext;
+        }
+        public void addToDB(string ad)
+        {
+            if (adContext.adModels.Count() > 10)
+            {
+                AdModel adModel = new AdModel();
+                adModel.ad = ad;
+                adContext.adModels.Add(adModel);
+                adContext.SaveChanges();
+            }
+        }
+        public void deleteFromDB(string ad)
+        {
+            if (!this.isEmpty())
+            {
+                adContext.adModels.Remove(adContext.adModels.Where(o => o.ad == ad).FirstOrDefault());
+                adContext.SaveChanges();
+            }
+        }
+        public bool isEmpty()
+        {
+            return adContext.adModels.Count() == 0;
+        }
     }
 }
