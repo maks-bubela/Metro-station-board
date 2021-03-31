026fdf90310003d85acba9a8bce24a04c14cdd2a
Maks Bubela <ya.bubela@gmail.com>
2021-03-31 18:09:25 +0300

    Added get set methods to string fields

--- Metro-station-board/Metro-station-board/Model/ScheduleModel.cs
+++ Metro-station-board/Metro-station-board/Model/ScheduleModel.cs
@@ -5,8 +5,8 @@ namespace Metro_station_board.Model
     public class ScheduleModel
     {
         public long Id { get; set; }
-        public string dispatchPoint;
-        public string endPoint;
+        public string dispatchPoint { get; set; }
+        public string endPoint { get; set; }
         public TimeSpan dispatchTime { get; set; }
         public TimeSpan arriveTime { get; set; }
     }
