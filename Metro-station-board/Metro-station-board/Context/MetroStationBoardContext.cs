48b9800decaa37d41a80bdda386f42fc71cbb782
Maks Bubela <ya.bubela@gmail.com>
2021-03-31 15:30:39 +0300

    Added one more constructor to context class

--- Metro-station-board/Metro-station-board/Context/MetroStationBoardContext.cs
+++ Metro-station-board/Metro-station-board/Context/MetroStationBoardContext.cs
@@ -8,6 +8,9 @@ namespace Metro_station_board.Context
         public MetroStationBoardContext(string connectionString) : base(connectionString)
         {
         }
+        public MetroStationBoardContext()
+        {
+        }
         public DbSet<AdModel> adModels { get; set; }
         public DbSet<ScheduleModel> scheduleModels { get; set; }
         public DbSet<ViolationModel> violationModels { get; set; }
