f39494469488297d7434a9062327f6fcc33a5976
Maks Bubela <ya.bubela@gmail.com>
2021-04-01 17:02:20 +0300

    Added new class ControlPanelObject

---
+++ Metro-station-board/Metro-station-board/Objects/ControlPanelObject.cs
@@ -0,0 +1,24 @@
+﻿using Metro_station_board.Context;
+using Metro_station_board.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Objects
+{
+    class ControlPanelObject
+    {
+        private AdRepository adRepository;
+        private ScheduleRepository scheduleRepository;
+        private VioletRepository violationRepository;
+
+        public ControlPanelObject(MetroStationBoardContext context)
+        {
+            adRepository = new AdRepository(context);
+            scheduleRepository = new ScheduleRepository(context);
+            violationRepository = new VioletRepository(context);
+        }
+    }
+}
