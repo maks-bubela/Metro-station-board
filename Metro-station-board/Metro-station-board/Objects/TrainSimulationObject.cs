f5f940cbb8f683fc46fb43b05ce9350a0b99458c
Maks Bubela <ya.bubela@gmail.com>
2021-04-03 13:53:13 +0300

    Added TrainSimulationObject class

---
+++ Metro-station-board/Metro-station-board/Objects/TrainSimulationObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Objects
+{
+    class TrainSimulationObject
+    {
+        ObjectSensor sensor;
+        ObjectTrain train;
+
+        public TrainSimulationObject()
+        {
+
+        }
+
+
+
+    }
+}
