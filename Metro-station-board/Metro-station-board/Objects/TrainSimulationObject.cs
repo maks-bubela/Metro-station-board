c0c1028854dac9068ee43864ecfbd3143c46675a
Maks Bubela <ya.bubela@gmail.com>
2021-04-03 15:30:14 +0300

    Added StartTrain method in TrainSimulationObject class

--- Metro-station-board/Metro-station-board/Objects/TrainSimulationObject.cs
+++ Metro-station-board/Metro-station-board/Objects/TrainSimulationObject.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Metro_station_board.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +11,36 @@ namespace Metro_station_board.Objects
     {
         ObjectSensor sensor;
         ObjectTrain train;
+        public ObjectSensor GetSensor() { return sensor; }
 
         public TrainSimulationObject()
         {
-
+            sensor = new ObjectSensor();
         }
 
+        public void StartTrain(string dispatchPoint, string endPoint, TimeSpan dispatchTime, TimeSpan arriveTime)
+        {
+            train = new ObjectTrain(dispatchPoint, endPoint);
+            train.trainInWay(true);
+            Random random = new Random();
+            TimeSpan violationDispatchTime = new TimeSpan(0,0,0);
+            if (random.Next(100) % 100 < 5)
+                violationDispatchTime = new TimeSpan(0, random.Next(5), 0);
+            sensor.startRide(dispatchTime + violationDispatchTime);
 
+            TimeSpan violationArriveTime = new TimeSpan(0,0,0);
+            if (random.Next(100) % 100 < 5)
+                violationArriveTime = new TimeSpan(0, random.Next(5), 0);
+            sensor.finishRide(arriveTime + violationArriveTime);
+            train.trainInWay(false);
+            
+            if(dispatchTime != sensor.GetDispatchTime() || arriveTime != sensor.GetArriveTime())
+            {
+                VioletRepository violetRepository = new VioletRepository(new Context.MetroStationBoardContext());
+                violetRepository.addToDB(sensor.GetDispatchTime(), sensor.GetArriveTime(),dispatchPoint,endPoint);
+            }
+
+        }
 
     }
 }
