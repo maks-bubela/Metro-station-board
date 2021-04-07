9ed265bbb8c9c15ae7a4ffe4797cde922633d85f
Maks Bubela <ya.bubela@gmail.com>
2021-04-07 16:09:17 +0300

    Added WeatherObject class

---
+++ Metro-station-board/Metro-station-board/Objects/WeatherObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Objects
+{
+    class WeatherObject
+    {
+        private int temperature;
+        private int precipitation;
+        private int airHumidity;
+        private int wind;
+
+        public WeatherObject(int temperature, int precipitation, int airHumidity, int wind)
+        {
+            this.temperature = temperature;
+            this.precipitation = precipitation;
+            this.airHumidity = airHumidity;
+            this.wind = wind;
+        }
+    }
+}
