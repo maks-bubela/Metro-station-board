edd365e45f879cfd77c39914edb46e20b3ebd5c6
Maks Bubela <ya.bubela@gmail.com>
2021-04-07 16:17:11 +0300

    Added ForecastObject class

---
+++ Metro-station-board/Metro-station-board/Objects/ForecastObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Objects
+{
+    class ForecastObject
+    {
+        WeatherObject weather;
+
+        public WeatherObject getForecast()
+        {
+            Random random = new Random();
+            weather = new WeatherObject(random.Next(-20,40), random.Next(0,100), random.Next(0, 100), random.Next(0, 40));
+            return weather;
+        }
+    }
+}
