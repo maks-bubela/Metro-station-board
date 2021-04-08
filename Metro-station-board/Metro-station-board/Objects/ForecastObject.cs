1541b35268c64e2747ec444faf5acf784b9b6b37
Maks Bubela <ya.bubela@gmail.com>
2021-04-08 15:53:15 +0300

    Added method getUI and inherited from IAddons interface

--- Metro-station-board/Metro-station-board/Objects/ForecastObject.cs
+++ Metro-station-board/Metro-station-board/Objects/ForecastObject.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Metro_station_board.Interfaces;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Metro_station_board.Objects
 {
-    class ForecastObject
+    class ForecastObject : IAddons
     {
         WeatherObject weather;
 
@@ -16,5 +20,29 @@ namespace Metro_station_board.Objects
             weather = new WeatherObject(random.Next(-20, 40), random.Next(0, 100), random.Next(0, 100), random.Next(0, 40));
         }
         public WeatherObject getForecast(){ return weather;}
+
+        public void getUI(Form form, MenuStrip addonsMenu)
+        {
+            Label temperature = new Label();
+            Label precipitation = new Label();
+            Label airHumidity = new Label();
+            Label wind = new Label();
+            int x = addonsMenu.Location.X;
+            int y = addonsMenu.Location.Y + addonsMenu.Size.Height;
+            ForecastObject forecast = new ForecastObject();
+            temperature.Location = new Point(x, y);
+            precipitation.Location = new Point(x + temperature.Size.Width, y);
+            y += temperature.Size.Height;
+            airHumidity.Location = new Point(x, y);
+            wind.Location = new Point(x + temperature.Size.Width, y);
+            temperature.Text = "Temperature : " + forecast.getForecast().GetTemp() + "°С";
+            precipitation.Text = "Precipitation : " + forecast.getForecast().GetPrecipitation() + "%";
+            airHumidity.Text = "Air Humidity : " + forecast.getForecast().GetAirHumidity() + "%";
+            wind.Text = "Wind : " + forecast.getForecast().GetWind() + " km/h";
+            form.Controls.Add(temperature);
+            form.Controls.Add(precipitation);
+            form.Controls.Add(airHumidity);
+            form.Controls.Add(wind);
+        }
     }
 }
