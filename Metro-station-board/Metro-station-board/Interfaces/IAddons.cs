6e6f1d84f26ed23771252ca29516c3515c606926
Maks Bubela <ya.bubela@gmail.com>
2021-04-08 15:51:17 +0300

    Added new interface for Addons

---
+++ Metro-station-board/Metro-station-board/Interfaces/IAddons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metro_station_board.Interfaces
+{
+    interface IAddons
+    {
+        void getUI(Form form, MenuStrip addonsMenu);
+    }
+}
