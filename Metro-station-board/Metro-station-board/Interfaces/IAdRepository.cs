de79159124a26ac6ac218be3db7ba1238867dc03
Maks Bubela <ya.bubela@gmail.com>
2021-04-10 16:47:19 +0300

    Added interfaces for all repositories

---
+++ Metro-station-board/Metro-station-board/Interfaces/IAdRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Interfaces
+{
+    interface IAdRepository
+    {
+        void addToDB(string ad);
+        void changeRecord(int id, string changeData);
+        void deleteFromDB(int id);
+        bool isEmpty();
+    }
+}
---
+++ Metro-station-board/Metro-station-board/Interfaces/IVioletRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Interfaces
+{
+    interface IVioletRepository
+    {
+        void addToDB(TimeSpan arriveTime, TimeSpan dispatchTime, string dispatchPoint, string endPoint);
+        void changeRecord(int id, string columnName, string changeData);
+        void changeRecord(int id, string columnName, TimeSpan timeSpan);
+        void deleteFromDB(int id);
+        bool isEmpty();
+    }
+}
