40b127bd755a41d93d91a1748140f98e1b1e19e3
Maks Bubela <ya.bubela@gmail.com>
2021-04-10 17:20:30 +0300

    Deleted coments and registered context

--- Metro-station-board/Metro-station-board/Configuration/AutofacConfig.cs
+++ Metro-station-board/Metro-station-board/Configuration/AutofacConfig.cs
@@ -2,30 +2,27 @@
 using Autofac.Core;
 using Autofac.Integration.Mvc;
 using Metro_station_board;
+using Metro_station_board.Context;
 using Metro_station_board.Interfaces;
 using Metro_station_board.Repository;
 using System.Web.Mvc;
 
 public class AutofacConfig
 {
-    public ContainerBuilder ConfigureContainer()
+    public static IContainer ConfigureContainer()
     {
-        // получаем экземпляр контейнера
         var builder = new ContainerBuilder();
 
-        // регистрируем контроллер в текущей сборке
         builder.RegisterControllers(typeof(MetroStationBoard).Assembly);
 
-        // регистрируем споставление типов
+        builder.Register(ctx => new MetroStationBoardContext()).AsSelf();
         builder.RegisterType<AdRepository>().As<IAdRepository>();
         builder.RegisterType<ScheduleRepository>().As<IScheduleRepository>();
         builder.RegisterType<VioletRepository>().As<IVioletRepository>();
 
-        // создаем новый контейнер с теми зависимостями, которые определены выше
         var container = builder.Build();
 
-        // установка сопоставителя зависимостей
         DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-        return builder;
+        return container;
     }
 }
