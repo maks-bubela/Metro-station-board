e2daacaa24d13842244ad5e6e7e8e2bd6e4ebbf7
Maks Bubela <ya.bubela@gmail.com>
2021-04-13 14:41:47 +0300

    Added new form with implementation for Train Simulation

---
+++ Metro-station-board/Metro-station-board/Train Simulator.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Metro_station_board
+{
+    partial class Train_Simulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dispatchLabel = new System.Windows.Forms.Label();
+            this.arriveLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(623, 369);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dispatchLabel
+            // 
+            this.dispatchLabel.AutoSize = true;
+            this.dispatchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dispatchLabel.Location = new System.Drawing.Point(641, 78);
+            this.dispatchLabel.Name = "dispatchLabel";
+            this.dispatchLabel.Size = new System.Drawing.Size(64, 25);
+            this.dispatchLabel.TabIndex = 3;
+            this.dispatchLabel.Text = "label1";
+            // 
+            // arriveLabel
+            // 
+            this.arriveLabel.AutoSize = true;
+            this.arriveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.arriveLabel.Location = new System.Drawing.Point(641, 103);
+            this.arriveLabel.Name = "arriveLabel";
+            this.arriveLabel.Size = new System.Drawing.Size(64, 25);
+            this.arriveLabel.TabIndex = 4;
+            this.arriveLabel.Text = "label2";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timerLabel.Location = new System.Drawing.Point(641, 128);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(64, 25);
+            this.timerLabel.TabIndex = 5;
+            this.timerLabel.Text = "label3";
+            // 
+            // Train_Simulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.arriveLabel);
+            this.Controls.Add(this.dispatchLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Train_Simulator";
+            this.Text = "Train_Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label dispatchLabel;
+        private System.Windows.Forms.Label arriveLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Metro-station-board/Metro-station-board/Train Simulator.cs
@@ -0,0 +1,64 @@
+﻿using Autofac;
+using Metro_station_board.Context;
+using Metro_station_board.Model;
+using Metro_station_board.Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metro_station_board
+{
+    public partial class Train_Simulator : Form
+    {
+        TimeSpan realTimeInWay;
+        public Train_Simulator()
+        {
+            InitializeComponent();
+            var container = AutofacConfig.ConfigureContainer();
+            var service = container.Resolve<MetroStationBoardContext>();
+            dataGridView1.DataSource = service.scheduleModels.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                string dispatchPoint = (dataGridView1.CurrentRow.Cells[1].Value.ToString());
+                string arrivePoint = (dataGridView1.CurrentRow.Cells[2].Value.ToString());
+                TimeSpan dispatchTime = TimeSpan.Parse(dataGridView1.CurrentRow.Cells[3].Value.ToString());
+                TimeSpan arriveTime = TimeSpan.Parse(dataGridView1.CurrentRow.Cells[4].Value.ToString());
+                TrainSimulationObject simulationObject = new TrainSimulationObject();
+                simulationObject.StartTrain(dispatchPoint, arrivePoint, dispatchTime, arriveTime);
+
+                realTimeInWay = simulationObject.GetSensor().GetArriveTime() - simulationObject.GetSensor().GetDispatchTime();
+                dispatchLabel.Text = dispatchPoint;
+                arriveLabel.Text = arrivePoint;
+
+                timer1.Enabled = true;
+                timer1.Interval = 1000;
+                timer1.Tick += timerTick;
+                timer1.Start();
+            }
+        }
+        public void timerTick(object sender, EventArgs e)
+        {
+            if (realTimeInWay.TotalSeconds > 0)
+            {
+                realTimeInWay -= new TimeSpan(0, 0, 1);
+                timerLabel.Text = realTimeInWay.ToString();
+            }
+            else
+            {
+                timer1.Tick -= timerTick;
+                timer1.Stop();
+            }
+        }
+    }
+}
