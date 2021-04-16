6be9133207e5478cbe4988c2e5972a94f670a0d6
Maks Bubela <ya.bubela@gmail.com>
2021-04-16 18:02:48 +0300

    Add module for service

---
+++ Metro-station-board/Metro-station-board/Modules/ServiceModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Metro_station_board.Interfaces;
+using Metro_station_board.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro_station_board.Modules
+{
+    class ServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DataService>().As<IDataService>();
+        }
+    }
+}
