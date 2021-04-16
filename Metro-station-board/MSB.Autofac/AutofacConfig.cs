b0dcaa393d9211c708fa36293d9c74a94a1ee353
Maks Bubela <ya.bubela@gmail.com>
2021-04-16 13:44:11 +0300

    Added new project for replace AutofacConfig class

---
+++ Metro-station-board/MSB.Autofac/AutofacConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Metro_station_board;
+using Metro_station_board.Context;
+using Metro_station_board.Interfaces;
+using Metro_station_board.Repository;
+using System.Web.Mvc;
+
+namespace MSB.Autofac
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MetroStationBoard).Assembly);
+
+            builder.Register(ctx => new MetroStationBoardContext()).AsSelf();
+            builder.RegisterType<AdRepository>().As<IAdRepository>();
+            builder.RegisterType<ScheduleRepository>().As<IScheduleRepository>();
+            builder.RegisterType<VioletRepository>().As<IVioletRepository>();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
