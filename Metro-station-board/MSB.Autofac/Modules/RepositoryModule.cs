a00865b1ab5eac28cc59a191db6836cec2ca3b08
Maks Bubela <ya.bubela@gmail.com>
2021-04-19 15:40:35 +0300

    Added RepositoryModule

---
+++ Metro-station-board/MSB.Autofac/Modules/RepositoryModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Metro_station_board.Interfaces;
+using Metro_station_board.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSB.Services.Modules
+{
+    public class RepositoryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AdRepository>().As<IAdRepository>();
+            builder.RegisterType<ScheduleRepository>().As<IScheduleRepository>();
+            builder.RegisterType<VioletRepository>().As<IVioletRepository>();
+        }
+    }
+}
